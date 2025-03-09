af95edd78ab41d60dc762163c35a534b82753d87
prince kumar <princekr969@gmail.com>
2025-03-09 23:38:38 +0530

    Fix GitHub Pages routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { Navbar, VideoSection, Carousel, Footer } from "./components"
-import { createBrowserRouter, createRoutesFromElements, BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
-import { Layout1 } from "./layouts"
-import { HomePage, OlympiadPage } from "./pages"
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout1 } from "./layouts";
+import { HomePage, OlympiadPage } from "./pages";
 
 function App() {
-
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <BrowserRouter basename={"https://princekr969.github.io/Sangillence"}>
-  {/* Routes Here */}
+  return (
+    <BrowserRouter basename="/Sangillence"> {/* 👈 Use basename here */}
       <Routes>
-        <Route path="/" element={<Layout1/>}>
-          <Route path='' element={<HomePage/>}></Route>
-          <Route path='olympiad' element={<OlympiadPage/>}></Route>
-          <Route path='contactUs' element={<OlympiadPage/>}></Route>
-          <Route path='nep2020' element={<OlympiadPage/>}></Route>
+        <Route path="/" element={<Layout1 />}>
+          <Route index element={<HomePage />} />
+          <Route path="olympiad" element={<OlympiadPage />} />
+          <Route path="contactUs" element={<OlympiadPage />} />
+          <Route path="nep2020" element={<OlympiadPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-
-     
-      )
-    )
-
-  return (
-    <>
-    <RouterProvider router={router}/>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
