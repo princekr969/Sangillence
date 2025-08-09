5d496049817838b6a58eb9300483c5457a1f78d7
prince kumar <princekr969@gmail.com>
2025-08-09 14:02:55 +0530

    update cors config

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? ["http://localhost:5173", "https://sangillence.com/"]
+        ? ["http://localhost:5173", "https://sangillence.com"]
         : ["http://localhost:5173", "http://localhost:3000"],
     credentials: true,
   })
