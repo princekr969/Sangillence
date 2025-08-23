d74f61666218635cc89e6868af375f1ea49226cb
prince kumar <princekr969@outlook.com>
2025-08-23 11:59:15 +0530

    remove nep page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import HomePage from "./HomePage";
 import OlympiadPage from "./OlympiadPage";
-import NEP2020 from "./NEP2020Page";
 import ContactUs from "./ContactUsPage";
 import LoadingPage from "./LoadingPage";
 import AboutPage from "./AboutPage";
@@ -8,7 +7,6 @@ import AboutPage from "./AboutPage";
 export {
     HomePage,
     OlympiadPage,
-    NEP2020,
     ContactUs,
     LoadingPage,
     AboutPage
